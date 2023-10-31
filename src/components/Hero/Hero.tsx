13c72928d99c1c8aa4dc1d859593c396023f561b
Elham Estedlali <e.estedlali@gmail.com>
2023-10-31 10:40:08 +0330

    Used tailwind for the CSS

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,20 +1,26 @@
-import React from "react";
 import backgroundImage from "../../assets/b1.jpg";
-import classes from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div className={[classes.hero, classes.container].join(" ")}>
+    <div className="text-center md:flex md:flex-row md:justify-around md:items-center">
       <div>
-        <h3>Serving Delightful Moments...</h3>
-        <h1>Aso Resturant</h1>
-        <h2>رستوران آسو</h2>
-        <button className={classes["hero-button"]}>View Menu</button>
+        <h3 className="text-sm font-medium text-thirdText mb-3.5 md:text-base">
+          لحظات خوشایندی را با ما تجربه کنید...
+        </h3>
+        <h1 className="text-3xl text-primaryText mb-2 mt-2 md:text-5xl md:mb-5 md:mt-3 ">
+          رستوران آسو
+        </h1>
+        <h2 className="text-base font-semibold text-thirdText md:text-xl md:mt-2 md:mb-2">
+          Aso Resturant
+        </h2>
+        <button className="btn border-none outline-none m-auto mt-5 text-sm font-semibold bg-primaryColor text-textIconColor hover:bg-darkPrimaryColor active:bg-darkPrimaryColor">
+          مشاهده منو
+        </button>
       </div>
       <img
         src={backgroundImage}
         alt="Aso Resturant"
-        className={classes["hero-image"]}
+        className="hidden mt-0 mb-0 mx-auto max-w-xl select-none md:block md:w-1/2"
       />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPrimaryColor: "#1d965c", //"#00796b",
+        primaryColor: "#32bb78", //"#009688"
+        lightPrimaryColor: "#b2dfdb",
+        accentColor: "#ffc107",
+        primaryText: "#212121",
+        secondaryText: "#757575",
+        thirdText: "#99a5a3",
+        deviderColor: "#bdbdbd",
+        textIconColor: "#ffffff",
+      },
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
